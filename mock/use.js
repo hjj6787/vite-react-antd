8045475fde2280bb1ba3d96d5e9718000202ab58
xiaohe23 <486158409@qq.com>
2024-03-30 18:59:27 +0800

    24330.路由信息修改

--- mock/use.js
+++ mock/use.js
@@ -12,11 +12,17 @@ export default [
                     "key":101,
                     "label":'op1',
                     "ps_icon":'StockOutlined',
+                    "ps_c":'linechart1',
+                    'ps_a':'',
+                    'ps_level':'0',
                     "children":[
                         {
                             "key":1011,
                             "label":'op1_1',
                             "ps_icon":'PieChartOutlined',
+                            "ps_c":'linechart1',
+                            'ps_a':'index',
+                            'ps_level':'1',
                         }
                     ]
                 },
@@ -24,11 +30,17 @@ export default [
                     "key":102,
                     "label":'op2',
                     "ps_icon":'DotChartOutlined',
+                    "ps_c":'linechart2',
+                    'ps_a':'',
+                    'ps_level':'0',
                     "children":[
                         {
                             "key":1021,
                             "label":'op2_1',
                             "ps_icon":'AimOutlined',
+                            "ps_c":'linechart2',
+                            'ps_a':'index',
+                            'ps_level':'1',
                         }
                     ]
                 },
@@ -36,11 +48,17 @@ export default [
                     "key":103,
                     "label":'op3',
                     "ps_icon":'CompassOutlined',
+                    "ps_c":'linechart2',
+                    'ps_a':'',
+                    'ps_level':'0',
                     "children":[
                         {
                             "key":1031,
                             "label":'op3_1',
                             "ps_icon":'ContactsOutlined',
+                            "ps_c":'linechart3',
+                            'ps_a':'index',
+                            'ps_level':'0',
                         }
                     ]
                 }
---
+++ src/pages/linechart3/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Linechart=()=>{
+
+
+    return <>
+        <h1>linechart3</h1>
+    </>
+}
+
+export default Linechart
