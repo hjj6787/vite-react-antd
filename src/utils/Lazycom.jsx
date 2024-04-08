0103d677bae8548dd165286aef26b5c73189f1ee
xiaohe23 <486158409@qq.com>
2024-04-08 19:10:46 +0800

    动态路由文件自动引入逻辑

--- src/utils/Lazycom.jsx
+++ src/utils/Lazycom.jsx
@@ -1,17 +1,25 @@
 // LazyLoad.tsx
 import { Suspense } from 'react'
 import loadable from '@loadable/component'
-const modules = import.meta.glob('@/src/pages/*/*.jsx')
+const modules = import.meta.glob('@/pages/*/*.jsx')
 const loadables = loadable
 
 function LazyLoad(url) {
-  const ComponentNode = loadables(async () => {
-    return modules[`/src/pages/${url}.jsx`]()
-  })
+  console.log(url);
+  const modulePath = `/src/pages/${url}.jsx`;
+
+  // 检查确保模块路径在对象中存在并且是一个函数
+  if (typeof modules[modulePath] !== 'function') {
+    console.error(`Module not found or not a function: ${modulePath}`);
+    return null;
+  }
+
+  const LoadableComponent = loadable(() => modules[modulePath]());
   return (
     <Suspense>
-      <ComponentNode />
+      <LoadableComponent />
     </Suspense>
+    // <h1>11</h1>
   )
 }
 export default LazyLoad
